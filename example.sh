c18ab4bb99aa2ccffb60314e91ec18ae4df2e3f8
Subhaditya Nath <sn03.general@gmail.com>
2021-08-12 21:29:47 +0530

    Improve example.sh

--- example.sh
+++ example.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
-if command -v inotifywait >/dev/null && command -v capsctrl >/dev/null
-then while true
-do capsctrl "/dev/input/by-path/$(inotifywait -q -e create --include '.*-event-kbd' --format '%f' --no-newline /dev/input/by-path)"
-done fi &
+WATCHDIR="${WATCHDIR-/dev/input/by-path}"
+WATCHDIR="$(echo "$WATCHDIR" | sed 's#/*$##')"
+
+[ -z "$WATCHDIR" ] && {
+	echo WATCHDIR cannot be empty >&2
+	exit 1
+}
+
+DEPS=
+DEPS="$DEPS capsctrl"
+DEPS="$DEPS inotifywait"
+
+for DEP in $DEPS
+do command -v $DEP >/dev/null || { echo "$DEP not found" >&2; exit 1; }
+done
+
+inotifywait_args=
+inotifywait_args="$inotifywait_args -q"
+inotifywait_args="$inotifywait_args -e create"
+inotifywait_args="$inotifywait_args --include '.*-event-kbd'"
+inotifywait_args="$inotifywait_args --format %f"
+inotifywait_args="$inotifywait_args --no-newline"
+
+while true
+do capsctrl "$WATCHDIR/$(inotifywait $inotifywait_args $WATCHDIR)" &
+done
+
 # vim: set nowrap ft=sh:
